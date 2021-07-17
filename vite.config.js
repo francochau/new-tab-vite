1ef60ad10dfce9fd30488e9735af672b9f0e65e0
Franco Chau <franco0506@gmail.com>
2021-07-17 20:08:12 +0800

    init proj

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import copy from 'rollup-plugin-copy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  manifest: true,
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    reactRefresh(),
+    copy({
+      targets: [{ src: './src/manifest.json', dest: 'dist' }],
+      hook: 'writeBundle',
+    }),
+  ],
+});
